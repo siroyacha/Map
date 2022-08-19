1e24738f68a84a881d314a2c78c92f09e3b7b940
jae_hyeok <a@a>
2022-08-19 20:44:29 +0900

    multimap

--- Map/Main.cpp
+++ Map/Main.cpp
@@ -1,4 +1,7 @@
+// ** 이터레이터 공부하기
 #include<iostream>
+#include<Windows.h>
+#include<math.h>
 #include<string>
 #include<map>
 #include<vector>
@@ -8,6 +11,7 @@ using namespace std;
 
 int main()
 {
+	/*
 	{
 		map<string, int>Number;
 
@@ -35,5 +39,161 @@ int main()
 
 		}
 	}
+	*/
+
+	{
+		/*
+		class Object
+		{
+		private:
+			string Key;
+			int Attack;
+		public:
+			void Initiailize() { Key = " "; Attack = rand() % 250 + 50; }
+			void Render() { cout << "공격력 : " << Attack << endl; }
+		public:
+			string GetKey() { return Key; }
+			void SetKey(const string& _key) { Key = _key; }
+			void SetAttack(const int& _Attack) { Attack = _Attack; }
+		};
+		*/
+		/*
+		{
+			map<string, list<Object*>> ObjecList;
+
+			for (int i = 0; i < 7; ++i)
+			{
+				Object* pObj = new Object;
+				pObj->Initiailize();
+
+				string str = " ";
+				cout << "이름 입력 : "; cin >> str;
+				pObj->SetKey(str);
+				map<string, list<Object*>>::iterator iter = ObjecList.find(str);
+
+				if (iter == ObjecList.end())
+				{
+					list<Object*> TempList;
+					TempList.push_back(pObj);
+
+					ObjecList.insert(make_pair(pObj->GetKey(), TempList));
+				}
+				else
+					iter->second.push_back(pObj);
+			}
+
+			cout << "입력 종료" << endl << endl;
+			cout << "탐색 시작" << endl;
+			while (true)
+			{
+				// ** map 자료 탐색
+				string str = "";
+				cout << "이름 입력 : "; cin >> str;
+
+				if (str == "q")
+					break;
+
+				map<string, list<Object*>>::iterator iter = ObjecList.find(str);
+
+				if (iter == ObjecList.end())
+				{
+					cout << "무기가 없습니다." << endl;
+					continue;
+				}
+				else
+				{
+					for (list<Object*>::iterator iter2 = iter->second.begin();
+						iter2 != iter->second.end(); ++iter2)
+					{
+						(*iter2)->Render();
+					//iter->second.front()->Render();
+
+					for (size_t i = 0; i < iter->second.size(); i++)
+					{
+						iter->second.front()->Render();
+						iter->second.push_back(iter->second.front());
+						iter->second.pop_front();
+					}
+
+					}
+				}
+			}
+			cout << "** 탐색 종료 **" << endl << endl;
+		}
+		*/
+
+	}
+
+	/*
+	{
+		multimap<string, int>strList;
+
+		strList.insert(make_pair("홍길동", 1));
+		strList.insert(make_pair("홍길동", 2));
+		strList.insert(make_pair("홍길동", 3));
+		strList.insert(make_pair("홍길동", 4));
+		strList.insert(make_pair("홍길동", 5));
+		strList.insert(make_pair("홍길동", 6));
+		strList.insert(make_pair("홍길동", 7));
+		strList.insert(make_pair("홍길동", 8));
+		strList.insert(make_pair("홍길동", 9));
+		strList.insert(make_pair("홍길동", 10));
+
+		for (multimap<string, int>::iterator iter = strList.begin();
+			iter != strList.end(); ++iter)
+		{
+			cout << iter->first << " : " << iter->second << endl;
+		}
+	}
+	*/
+
+	int i = 0;
+
+	{
+		class Vector3
+		{
+		public:
+			float x, y, z;
+			Vector3() {}
+			Vector3(float _x, float _y) :x(_x), y(_y), z(0) {}
+			Vector3(float _x, float _y, float _z) :x(_x), y(_y), z(_z) {}
+		};
+		class object
+		{
+		private:
+			string Key;
+			Vector3 Position;
+			object* Target;
+		public:
+			void Initialize()
+			{
+				Key = " ";
+				Target = nullptr;
+				srand(GetTickCount64());
+				Position = Vector3(
+					rand() % 50 + 1,
+					rand() % 50 + 1);
+			}
+
+			void Render()
+			{
+				cout << "X : " << Position.x << endl;
+				cout << "Y : " << Position.y << endl;
+			}
+		public:
+			string GetKey() { return Key; }
+			void SetKey(const string& _key) { Key = _key; }
+			//void SetTarget(const object& _Target) { Target = _Target; }
+			Vector3 GetPosition()const { return Position; }
+			void SetPosition(const float& _x, const float& _y) { Position = Vector3(_x, _y); }
+		};
+		object* Player = new object;
+		Player->SetPosition(25.0f, 25.0f);
+
+		multimap<float, object*> ObjectList;
+	}
+
+
+
 	return 0;
 }
